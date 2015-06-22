07f99cdbfa4a9a73e880d2799d8293ea9b6c1a30
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-06-22 09:27:12 +0300

    fix horizontal scrolling

--- build/js/main.js
+++ build/js/main.js
@@ -16147,8 +16147,8 @@ $(document).ready(function() {
             nav: true,
             dots:false,
             items: 1,
-            animateOut: 'slideInLeft',
-            animateIn: 'slideInRight',
+            //animateOut: 'slideInLeft',
+            //animateIn: 'slideInRight',
             navText: [
                 "<img class='slider__btn' src='img/images/slider-btn.png'>",
                 "<img class='slider__btn slider__btn--next' src='img/images/slider-btn.png'>"
